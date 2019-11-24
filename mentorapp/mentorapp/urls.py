9ea3d1f565cfcde947f6448d4c9bfba0d29ed0b2
Natalia <natalia@Roberts-MacBook-Pro.local>
2019-11-24 11:51:31 -0500

    created main page

--- mentorapp/mentorapp/urls.py
+++ mentorapp/mentorapp/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('mainpage/', include('mainpage.urls')),
     path('register/', include('register.urls')),
     path('admin/', admin.site.urls),
     path('mentorsearch/', include('mentorsearch.urls')),
