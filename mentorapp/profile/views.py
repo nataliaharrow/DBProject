50fecf34cd3a9500c3f7a912ce64406cb0a82cce
fatsoflo <fatsoflo@users.noreply.github.com>
2019-12-13 20:02:03 -0500

    fixed profile bug

--- mentorapp/profile/views.py
+++ mentorapp/profile/views.py
@@ -5,18 +5,23 @@ from app_user.models import *
 # Create your views here.
 
 def profile(request, pk):
-    # if pk != request.user.pk:
-    #     if request.method == 'POST':
-    #         body = parse_req_body(request)
-    #         if body['action'] == edit_profile:
-    print(pk)
-    print(request.user.pk)
-    print(request)
-    user = User.objects.get(pk=pk)
-    profile = Profile.objects.get(user=user)
+    profile_user = User.objects.get(pk=pk)
+    
+    if pk != request.user.pk:
+        if request.method == 'POST':
+            body = parse_req_body(request)
+            if body['action'] == "Connect":
+                if request.user.is_student:
+                    Connection.objects.create(student=request.user, mentor=profile_user, request_from="S",status="P")
+                elif request.user.is_mentor:
+                    Connection.objects.create(student=profile_user, mentor=request.user, request_from="M",status="P")
+    
+    
+    profile = Profile.objects.get(user=profile_user)
     context = { 
         'profile': profile,
-        'pk': pk
+        'profile_user':profile_user,
+        'pk': pk,
     }
     return render(request, 'profile/profile.html', context=context)        
 
