3cf82c44b703cde6b2c56e740250c8c2bf5fae9c
fatsoflo <fatsoflo@users.noreply.github.com>
2019-12-13 19:51:55 -0500

    changed profile to view different functions depending on who's viewing

--- mentorapp/profile/views.py
+++ mentorapp/profile/views.py
@@ -3,14 +3,28 @@ from helper import parse_req_body
 from app_user.models import *
 
 # Create your views here.
-def view_user_profile(request):
+
+def profile(request, pk):
+    # if pk != request.user.pk:
+    #     if request.method == 'POST':
+    #         body = parse_req_body(request)
+    #         if body['action'] == edit_profile:
+    print(pk)
+    print(request.user.pk)
+    print(request)
+    user = User.objects.get(pk=pk)
+    profile = Profile.objects.get(user=user)
+    context = { 
+        'profile': profile,
+        'pk': pk
+    }
+    return render(request, 'profile/profile.html', context=context)        
+
+def edit(request):
     user = request.user
     profile = Profile.objects.get(user=user)
-    user = request.user
-    profile = Profile.objects.get(user=user)
-    context = { 'profile': profile }
-    return render(request, 'profile/profile.html', context=context)
-
-
-
+    context = { 
+        'profile': profile,
+    }
+    return render(request, 'profile/edit.html', context=context)        
 
