599913802c22d92d01d77058cdef2a8dc441203c
fatsoflo <fatsoflo@users.noreply.github.com>
2019-12-16 14:31:22 -0500

    added mentor response pg, changed model to have foreign keys

--- mentorapp/app_user/admin.py
+++ mentorapp/app_user/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User, Profile, School, Major, Company, Industry, Request, Connection #, UserSchool, UserMajor, UserCompany, UserIndustry, UserRequest, CompanyIndustry
+from .models import *
 
 admin.site.register(User)
 admin.site.register(Request)
@@ -9,11 +9,6 @@ admin.site.register(School)
 admin.site.register(Major)
 admin.site.register(Company)
 admin.site.register(Industry)
-# admin.site.register(UserRequest)
-# admin.site.register(UserSchool)
-# admin.site.register(UserMajor)
-# admin.site.register(UserCompany)
-# admin.site.register(UserIndustry)
-# admin.site.register(CompanyIndustry)
+admin.site.register(UserRequest)
 admin.site.register(Connection)
 
